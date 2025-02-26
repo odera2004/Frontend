9178a5b2a4d7830562dd946588f931d51910c4d3
hafsa-0-abdy <hafsaabdirizack0@gmail.com>
2025-02-26 12:54:55 +0300

    Updated Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,16 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid bg-dark"> 
-        <Link className="navbar-brand text-light d-flex align-items-center" to="/"> 
+    <nav className="navbar navbar-expand-lg bg-dark">
+      <div className="container-fluid"> 
+        <NavLink className="navbar-brand text-light d-flex align-items-center" to="/"> 
           <img src={logo} alt="Logo" width="50" height="50" />
           <span className="ms-2">Auto Fix Hub</span> 
-        </Link>
+        </NavLink>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler border-white"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
@@ -18,33 +18,33 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon" style={{ filter: "invert(1)" }}></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link text-light" to="/">Home</Link>
+              <NavLink className="nav-link text-light" to="/" activeClassName="active">Home</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-light" to="/vehicle">Vehicle</Link>
+              <NavLink className="nav-link text-light" to="/vehicle" activeClassName="active">Vehicle</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-light" to="/work">Work</Link>
+              <NavLink className="nav-link text-light" to="/work" activeClassName="active">Work</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-light" to="/billings">Billings</Link>
+              <NavLink className="nav-link text-light" to="/billings" activeClassName="active">Billings</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-light" to="/book">Book</Link>
+              <NavLink className="nav-link text-light" to="/book" activeClassName="active">Book</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-light" to="/profile">Profile</Link>
+              <NavLink className="nav-link text-light" to="/profile" activeClassName="active">Profile</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-light" to="/login">Login</Link>
+              <NavLink className="nav-link text-light" to="/login" activeClassName="active">Login</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-light" to="/register">Register</Link>
+              <NavLink className="nav-link text-light" to="/register" activeClassName="active">Register</NavLink>
             </li>
           </ul>
         </div>
